d830d3661026a2f786dabcbf25d6c121b40001ba
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-29 21:33:22 -0700

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,42 @@
 class Instructor
+  attr_accessor :name 
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def tests 
+    BoatingTest.all.select {|test| test.instructor == self}
+  end
+
+ 
+
+  def pass_student(student, test_name)
+    student_test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+    if student_test 
+      student_test.test_status = "passed"
+    else 
+      BoatingTest.new(student, test_name, "passed", self)
+     end 
+  end 
+
+  def fail_student(student, test_name)
+    student_test = BoatingTest.all.find {|test| test.student == student && test.test_name == test_name}
+    if student_test 
+      student_test.test_status = "failed"
+    else 
+      BoatingTest.new(student, test_name, "failed", self)
+     end 
+  end 
+
+  def all_students
+    self.tests.map {|test| test.student}.uniq
+  end 
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,35 @@
 class Student
+  attr_accessor :first_name
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end 
+
+  def tests 
+    BoatingTest.all.select {|test| test.student == self}
+  end
+
+  def all_instructors
+    self.tests.map {|test| test.instructor}.uniq
+  end 
+
+  def self.find_student(name)
+    self.all.find {|person| person.first_name == name}
+  end 
+
+  def grade_percentage
+    pass = self.tests.count {|test| test.test_status == "passed"}
+    pass.to_f/self.tests.length.to_f * 100
+  end 
 
 end
